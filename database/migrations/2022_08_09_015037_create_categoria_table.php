720cf252d95970d9883a4018b2143e1246730f21
Jorge Mendes <jljmendesmanutencao@gmail.com>
2022-08-08 23:11:11 -0300

    Criando a tabela Categoria

---
+++ database/migrations/2022_08_09_015037_create_categoria_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categoria', function (Blueprint $table) {
+            $table->id();
+            $table->string('titulo')->nullable(false);
+            $table->string('cor')->default('#FFFFFF');
+            $table->foreignIdFor(User::class)->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('categoria', function(Blueprint $table){
+            $table->dropForeignIdFor(User::class);
+        });
+        Schema::dropIfExists('categoria');
+    }
+};
